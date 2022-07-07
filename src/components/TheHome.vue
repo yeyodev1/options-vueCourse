72f2aa02a9a03172fb19ecfd67183bcd16c9035d
Diego Reyes <diegorele13@gmail.com>
2022-07-07 16:45:04 -0500

    watch class

--- src/components/TheHome.vue
+++ src/components/TheHome.vue
@@ -3,12 +3,19 @@
 </template>
 
 <script>
-import { reactive } from "vue";
+import { reactive, watch } from "vue";
 
 export default {
   setup() {
     const obj = reactive({ counter: 0 });
     setInterval(() => obj.counter++, 500);
+
+    watch(
+      () => obj.counter,
+      (value, oldValue) => {
+        console.log(value, oldValue);
+      }
+    );
 
     return {
       obj,
